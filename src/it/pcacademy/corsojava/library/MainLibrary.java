2bd5f2cd532bc85b812c7d95467e5d3f42c178e1
emanuelecochi <lele.co@tiscali.it>
2016-01-12 23:16:43 +0100

    TreeMap

--- src/it/pcacademy/corsojava/library/MainLibrary.java
+++ src/it/pcacademy/corsojava/library/MainLibrary.java
@@ -4,8 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 public class MainLibrary {
 
@@ -16,34 +17,43 @@ public class MainLibrary {
 			File inputFile = new File("file/elencobibliotechelazio2014.csv");
 			BufferedReader buf = new BufferedReader(new FileReader(inputFile));	
 			
-			List<String[]> libraryArrayLazio = new ArrayList<String[]>();
+			List<TreeMap<String, String>> libraryArrayLazio = new ArrayList<TreeMap<String, String>>();
 			String line = buf.readLine();
+			String[] titleItems = line.split(",");
+			System.out.println(titleItems.length);
 			StringSplitLibrary infoArray = new StringSplitLibrary();
+			line = buf.readLine();
 			
 			while (line != null) {
-				libraryArrayLazio.add(infoArray.split(line, 16));
+				for (int i = 0; i < titleItems.length; i++) {
+					TreeMap<String, String> map = new TreeMap<String, String>();
+					map.put(titleItems[i], infoArray.split(line, titleItems.length)[i]);
+					libraryArrayLazio.add(map);
+				}
 				line = buf.readLine();
 			}
 			buf.close();
-			for (String[] library : libraryArrayLazio) {
-				System.out.println(Arrays.toString(library));
+			for (TreeMap<String, String> map : libraryArrayLazio) {
+				for (Entry<String,String> e: map.entrySet()) {
+					System.out.println(e.getKey() + ": " + e.getValue());
+				}
 			}
 			
-			List<Library> libraryList = new ArrayList<Library>();
-			for (String[] data: libraryArrayLazio) {
-				Library library = new Library();
-//				short[] cap = new short[5];
-//				for (int i = 0; i < data[8].length(); i++) {
-//					cap[i] = (short)Integer.parseInt(String.valueOf(data[9].charAt(i)));
-//				}
-				Address address = new Address(data[7], data[8], data[9]);
-				library.setIndirizzo(address);
-				libraryList.add(library);
-			}
-			
-			for (Library i : libraryList) {
-				System.out.println(i.getIndirizzo().getCap());
-			}
+//			List<Library> libraryList = new ArrayList<Library>();
+//			for (String[] data: libraryArrayLazio) {
+//				Library library = new Library();
+////				short[] cap = new short[5];
+////				for (int i = 0; i < data[8].length(); i++) {
+////					cap[i] = (short)Integer.parseInt(String.valueOf(data[9].charAt(i)));
+////				}
+//				Address address = new Address(data[7], data[8], data[9]);
+//				library.setIndirizzo(address);
+//				libraryList.add(library);
+//			}
+//			
+//			for (Library i : libraryList) {
+//				System.out.println(i.getIndirizzo().getCap());
+//			}
 			
 		} catch (Exception e) {
 			// stampa il messaggio di errore
